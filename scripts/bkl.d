f82738b7501a47d955aa2e285be26caf7e7d0a56
paul.d.fox <paul.d.fox@gmail.com>
2010-10-14 00:00:00 +0100

    /home/fox/release/dtrace/2010/dtrace-20101014.tar.bz2

---
+++ scripts/bkl.d
@@ -0,0 +1,26 @@
+/* Script to monitor calls to the BKL - Big Kernel Lock. */
+
+#pragma D option quiet
+int waiting, tcnt;
+long time_start;
+long time_tot;
+
+fbt::lock_kernel:entry
+{
+	time_start = timestamp;
+	waiting++;
+	tcnt++;
+}
+fbt::unlock_kernel:entry
+{
+	time_tot += waiting ? timestamp - time_start : 0;
+	waiting > 0 ? waiting-- : 0;
+}
+tick-1sec
+{
+	printf("Locks: %4d Total: %4d  Time/per lock: %4dus  Lock time: %5dus\n", 
+		waiting, tcnt, time_tot / tcnt, time_tot);
+}
+END {
+	printf("Locks acquired: %d\n", tcnt);
+}
